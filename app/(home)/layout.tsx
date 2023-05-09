5a10d1eedc6f09092e0b64c4e49aac9b6f2090d2
Niclas <me@niclas.lol>
2023-05-09 15:26:04 +0200

    feat: Navbar

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function HomeLayout({
     children,
@@ -6,6 +8,23 @@ export default function HomeLayout({
 }) {
     return (
         <>
+            <div className="border-b">
+                <div className="flex justify-between items-center py-2 lg:max-w-5xl lg:m-auto lg:px-0 px-2">
+                    <div>
+                        <h1 className="font-bold text-xl">Inhalt</h1>
+                    </div>
+
+                    <div className="space-x-2">
+                        <Link href="/auth/signin">
+                            <Button variant="ghost">Sign In</Button>
+                        </Link>
+                        <Link href="/auth/signin">
+                            <Button>Sign Up</Button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
             {children}
         </>
     )
